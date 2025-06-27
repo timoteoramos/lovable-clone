05aba7acac710458e715266ccef7dfda4c5ec219
sushantg <sushantg@vellestudio.com>
2025-06-27 12:10:08 +0530

    e2b integration

--- README.md
+++ README.md
@@ -14,14 +14,13 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
-## Learn More
+## Publish your e2b template
 
-To learn more about Next.js, take a look at the following resources:
+```bash
+e2b template build --name template-name --cmd "/compile_page.sh"
+```
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+name must be unique
 
 ## Deploy on Vercel
 
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,22 +1,38 @@
-import { Agent, openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { createAgent, openai } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("lovable-clone-nextjs-sg-0206");
+      return sandbox.sandboxId;
+    });
+
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: "code-agent",
-      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple next.js & React snippets.",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable code. You write simple next.js & React snippets.",
       model: openai({ model: "gpt-4o" }),
     });
 
     // Run the agent with an input.  This automatically uses steps
     // to call your AI model.
-    const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
